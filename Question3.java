c7a33873e221ad505a93f10e83a6eea26496ffd5
Natalielwr <130948572+Natalielwr@users.noreply.github.com>
2023-04-17 11:45:38 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -2,7 +2,15 @@ import java.util.Scanner;
 
 public class Question3
 {
-  Scanner in = new Scanner(System.in);
+  public static void main(String[] args)
+  {
+    /**
+     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
+     * 
+     * Hint: in.nextInt() to read integer
+     */
+
+    Scanner in = new Scanner(System.in);
     
     System.out.print("Enter a number: ");
     int num = in.nextInt();
